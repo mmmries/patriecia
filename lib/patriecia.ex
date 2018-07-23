9ff535a0eeb7bcebe452c71736bf93da7dbb4ed1
Michael Ries <michael@riesd.com>
2018-07-22 20:40:49 -0600

    initial

---
+++ lib/patriecia.ex
@@ -0,0 +1,18 @@
+defmodule Patriecia do
+  @moduledoc """
+  Documentation for Patriecia.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Patriecia.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/patriecia_test.exs
@@ -0,0 +1,8 @@
+defmodule PatrieciaTest do
+  use ExUnit.Case
+  doctest Patriecia
+
+  test "greets the world" do
+    assert Patriecia.hello() == :world
+  end
+end
