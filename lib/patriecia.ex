a374592a1e485b319e70ba93d184eac87be3e2be
Michael Ries <michael@riesd.com>
2018-07-23 00:00:44 -0600

    get the basics of building the trie working

--- lib/patriecia.ex
+++ lib/patriecia.ex
@@ -1,18 +1,51 @@
 defmodule Patriecia do
-  @moduledoc """
-  Documentation for Patriecia.
-  """
+  defstruct [:root]
 
-  @doc """
-  Hello world.
+  def new, do: %__MODULE__{}
 
-  ## Examples
-
-      iex> Patriecia.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def add(%__MODULE__{root: root}=trie, key, record) do
+    root = do_add(root, key, record)
+    %{trie | root: root}
   end
+
+  defp do_add(nil, key, record) do
+    %{part: key, values: MapSet.new([record])}
+  end
+  defp do_add(%{part: part}=node, part, record) do
+    %{node | values: MapSet.put(node.values, record)}
+  end
+  defp do_add(%{part: part}=node, key, record) do
+    cond do
+      String.starts_with?(key, part) ->
+        add_child(node, key, record)
+      true ->
+        shared_part = find_shared_part(part, key)
+        %{part: shared_part, values: MapSet.new()}
+        |> re_add_child(part, node.values)
+        |> add_child(key, record)
+    end
+  end
+
+  defp add_child(%{part: part}=node, key, record) do
+    part_size = byte_size(part)
+    rest = :binary.part(key, {part_size, byte_size(key) - part_size})
+    << next, _ :: binary >> = rest
+    Map.put(node, next, do_add(Map.get(node, next), rest, record))
+  end
+
+  defp re_add_child(%{part: part}=node, key, records) do
+    part_size = byte_size(part)
+    rest = :binary.part(key, {part_size, byte_size(key) - part_size})
+    << next, _ :: binary >> = rest
+    Map.put(node, next, %{part: rest, values: records})
+  end
+
+  defp find_shared_part(bin1, bin2) do
+    :binary.part(bin1, 0, find_shared_size(bin1,bin2))
+  end
+
+  defp find_shared_size(<<next , rest1::binary>>, <<next, rest2::binary>>) do
+    1 + find_shared_part(rest1, rest2)
+  end
+  defp find_shared_size(_, _), do: 0
 end
--- test/patriecia_test.exs
+++ test/patriecia_test.exs
@@ -2,7 +2,44 @@ defmodule PatrieciaTest do
   use ExUnit.Case
   doctest Patriecia
 
-  test "greets the world" do
-    assert Patriecia.hello() == :world
+  test "an empty trie" do
+    assert Patriecia.new() |> Map.get(:root) == nil
+  end
+
+  test "a trie with a single node" do
+    trie = Patriecia.new() |> Patriecia.add("key", 1)
+    assert trie.root == %{part: "key", values: MapSet.new([1])}
+  end
+
+  test "inserting another record with a duplicate key" do
+    trie = Patriecia.new() |> Patriecia.add("key", 1) |> Patriecia.add("key", 2)
+    assert trie.root == %{part: "key", values: MapSet.new([1, 2])}
+  end
+
+  test "a trie with overlapping keys" do
+    trie =
+      Patriecia.new()
+      |> Patriecia.add("key", 1)
+      |> Patriecia.add("keyper", 2)
+
+    assert trie.root == %{
+             :part => "key",
+             :values => MapSet.new([1]),
+             ?p => %{part: "per", values: MapSet.new([2])}
+           }
+  end
+
+  test "a trie with disjoint keys" do
+    trie =
+      Patriecia.new()
+      |> Patriecia.add("key", 1)
+      |> Patriecia.add("zoo", 2)
+
+    assert trie.root == %{
+             :part => "",
+             :values => MapSet.new(),
+             ?k => %{part: "key", values: MapSet.new([1])},
+             ?z => %{part: "zoo", values: MapSet.new([2])}
+           }
   end
 end
